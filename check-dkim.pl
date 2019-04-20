06758949bea7125eb481068469df1509392019b4
Anders Einar Hilden <hildenae@gmail.com>
2019-04-20 21:29:26 +0200

    Refactor code to functions and auto indent code

--- check-dkim.pl
+++ check-dkim.pl
@@ -10,10 +10,16 @@ use Sys::Syslog qw(:standard :macros);
 
 BEGIN {
     openlog($LOG_NAME, "ndelay,pid", "local0") or die("$LOG_NAME: Critical error, failed to connect to syslog");
-    $SIG{'__WARN__'} = sub { syslog( LOG_WARNING, @_ ); warn @_ };
-    $SIG{'__DIE__'}  = sub { syslog( LOG_ERR, @_ ); die @_ };
+    $SIG{'__WARN__'} = sub {
+        syslog(LOG_WARNING, @_);
+        warn @_
+    };
+    $SIG{'__DIE__'} = sub {
+        syslog(LOG_ERR, @_);
+        die @_
+    };
 }
-END { closelog(); }
+END {closelog();}
 
 use Fcntl; # O_RDONLY
 use Cwd;
@@ -21,7 +27,28 @@ use Cwd;
 use Email::MIME;
 use Mail::DKIM::Verifier;
 use JSON;
+use Email::Address::List;
+use List::Util qw(uniq);
 
+my %email_data;
+
+#my %random = (
+#        "Leonardo" => '1123 Fib Place',
+#        "Utako"   => 'Cantor Å Hotel, Room 1',
+#    );
+
+#my @cats = ( 'Daisy', 'Petunøia', 'Tuxedo', "...", 1, 9191919191919191919191919191 );
+
+#$cats[5] = '9191919191919191919191919191';
+
+#$email_data{hash} = \%random;
+#$email_data{array} = \@cats;
+
+#my $utf8_encoded_json_text = to_json(\%email_data, {pretty => 1});
+
+#say $utf8_encoded_json_text;
+
+#die("End of test");
 
 my $dkim_obj = Mail::DKIM::Verifier->new();
 my $message;
@@ -39,54 +66,81 @@ while (<>) {
     $dkim_obj->PRINT("$_\015\012");
 }
 
-die("Message was not filled from stdin or file, dies") unless($message);
+die("Message was not filled from stdin or file, dies") unless ($message);
 
 $dkim_obj->CLOSE;
 
-my $result = $dkim_obj->result;
+sub get_dkim_data {
+    my $dkim_obj = shift;
+    my %data;
+    $data{result} = $dkim_obj->result;
+    $data{result_full} = $dkim_obj->result_detail;
 
-print("DKMIM:");
-print("\ttesult: " . $result . "\n");
+    # there might be multiple signatures, what is the result per signature?
+    #foreach my $signature ($dkim_obj->signatures)
+    #{
+    #    print "\t\tsignature identity: " . $signature->identity . ', verify result: ' . $signature->result_detail . "\n";
+    #}
 
-if ($result ne "pass") {
-    my $detail = $dkim_obj->result_detail;
-    print("\t\tresult details: " . $detail . "\n");
-}
-# there might be multiple signatures, what is the result per signature?
-foreach my $signature ($dkim_obj->signatures)
-{
-    print "\t\tsignature identity: " . $signature->identity . ', verify result: ' . $signature->result_detail . "\n";
-}
-
-# the alleged author of the email may specify how to handle email
-print Dumper($dkim_obj->policies);
-foreach my $policy ($dkim_obj->policies)
-{
+    # the alleged author of the email may specify how to handle email
+    #print Dumper($dkim_obj->policies);
+    #foreach my $policy ($dkim_obj->policies)
+    #{
     # https://metacpan.org/pod/Mail::DKIM::DkimPolicy
     # Mail::DKIM::DkPolicy
     # Mail::DKIM::AuthorDomainPolicy
-    print $policy->location();
-    print ("Policy: " . $policy->apply($dkim_obj) . "\n");
-    print "\t\tWARNING: fraudulent message" if ($policy->apply($dkim_obj) eq 'reject');
+    #    print $policy->location();
+    #    print ("Policy: " . $policy->apply($dkim_obj) . "\n");
+    #    print "\t\tWARNING: fraudulent message" if ($policy->apply($dkim_obj) eq 'reject');
+    #}
+
+    return \%data;
 }
-print("Email headers:\n");
-my $email = Email::MIME->new($message);
 
-my @parts = $email->parts; # These will be Email::MIME objects, too.
-my $decoded = $email->body;
-my $non_decoded = $email->body_raw;
+sub get_email_data {
+    my $message = shift;
+    my %data;
+    my $email_obj = Email::MIME->new($message);
+    my $header_obj = $email_obj->header_obj;
+    $data{"DKIM-Signature"} = $header_obj->header_raw("DKIM-Signature");
 
-my $content_type = $email->content_type;
-my $description = $email->debug_structure;
+    $data{"To"} = ();
+    $data{"To"}{"raw"} = $header_obj->header_raw("to");
+    my @tos = Email::Address::List->parse(
+        $header_obj->header_raw("to"),
+        skip_comments => 1,
+        skip_groups   => 1,
+        skip_unknown  => 1
+    );
+    foreach my $e (@tos) {
+        if ($e->{'type'} eq 'mailbox') {
+            $data{"To"}{"host"} = $e->{'value'}->host;
+            $data{"To"}{"mailbox"} = $e->{'value'}->user;
+            (my $user, my $uuid) = split /\+/, $e->{'value'}->user;
+            $data{"To"}{"uuid"} = $uuid;
+            $data{"To"}{"user"} = $user;
+        }
+    } #$data{"To_parsed"} = @tos;
 
-#$description =~  s/^/\t\t/g;
-#$description =~  s/\n/\n\t\t/g;
-$description =~  s/^ *\+/\t\t/g;
-$description =~  s/^ ? *\+/\t\t\t/gm;
-chomp $description;
+    #my @parts = $email->parts; # These will be Email::MIME objects, too.
+    #my $decoded = $email->body;
+    #my $non_decoded = $email->body_raw;
 
-print "\tcontent_type: " . $content_type . "\n";
-print "\temail structure:\n" . $description . "\n";
+    #my $content_type = $email->content_type;
+    #my $description = $email->debug_structure;
+
+    #$description =~  s/^/\t\t/g;
+    #$description =~  s/\n/\n\t\t/g;
+    #$description =~  s/^ *\+/\t\t/g;
+    #$description =~  s/^ ? *\+/\t\t\t/gm;
+    #chomp $description;
+
+    #print "\tcontent_type: " . $content_type . "\n";
+    #print "\temail structure:\n" . $description . "\n";
+
+
+    return \%data;
+}
 
 sub get_dkim_signed_headers {
     my ($header_obj) = @_;
@@ -104,18 +158,24 @@ sub get_dkim_signed_headers {
 }
 
 #my @pairs = $parsed->header_str_pairs;
-my $header = $email->header_obj;
-use Data::Dumper;
+#my $header = $email->header_obj;
+#use Data::Dumper;
 
 # DKIM-Signature h=Date:To:Subject:From:MIME-Version:Content-Type;
 
 #print $header->as_string;
-my @vhead = get_dkim_signed_headers($header);
+#my @vhead = get_dkim_signed_headers($header);
 
-foreach my $hname (@vhead) {
-    my $hdata = $header->header_str($hname);
-    print "VALIDATED: " . $hname . ": " . $hdata . "\n" if $hdata;
-}
+#foreach my $hname (@vhead) {
+#    my $hdata = $header->header_str($hname);
+#    print "VALIDATED: " . $hname . ": " . $hdata . "\n" if $hdata;
+#}
 
+$email_data{"DKIM"} = get_dkim_data $dkim_obj;
+$email_data{"EMAIL"} = get_email_data $message;
 
-print("\n\n-----------------------------------------------------------------------\n");
+my $email_data_json = to_json(\%email_data, { pretty => 1 });
+
+say $email_data_json;
+
+print("-----------------------------------------------------------------------\n");
